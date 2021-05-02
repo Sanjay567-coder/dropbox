9d243e6405076e6f738120086240413ac8ac22fb
Sanjay Srinivas <72558978+Sanjay567-coder@users.noreply.github.com>
2021-05-02 06:35:16 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+import dropbox
+import os
+
+
+class TransferData:
+    def __init__(self, access_token):
+        self.aT = access_token
+
+    def upload_file(self, file_from, file_to):
+        dbx = dropbox.Dropbox(self.access_token)
+        fileName = os.path.split(file_from)[1]
+
+        dropbox_file = '/CloudStorage/'+fileName
+
+        with open(file_from, "rb") as f:
+            dbx.files_upload(f.read(), dropbox_file,
+                            mode=dropbox.files.WriteMode.overwrite)
+
+
+AToken = "sl.Av__rRKxl7D6OB63anvJVW6W-WP5NSQpiSUneoAmA3VoCBPjIDUAfTgMs-ap7wTU2iu0qKuhRo56N2Ca3SMQzlbu3OCQU7PQPBVSGUlZigLppSoouhGu7I0ep-FgM-G8EyMy-ug"
+
+cloudStoring = TransferData(access_token)
+
+while(os.path.isfile(file_from) == False):
+    fileFrom = input("Enter the file path to transfer:- ")
+
+cloudStoring.upload_file(fileFrom)
+
+print("File had been moved!)")
