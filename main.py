9a0989c61475df59c8dc76adef8ba480d33d064f
Sanjay Srinivas <72558978+Sanjay567-coder@users.noreply.github.com>
2021-05-04 18:52:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,14 +16,18 @@ class TransferData:
             dbx.files_upload(f.read(), dropbox_file,
                             mode=dropbox.files.WriteMode.overwrite)
 
+def main():
+    
+    AToken = "sl.Av__rRKxl7D6OB63anvJVW6W-WP5NSQpiSUneoAmA3VoCBPjIDUAfTgMs-ap7wTU2iu0qKuhRo56N2Ca3SMQzlbu3OCQU7PQPBVSGUlZigLppSoouhGu7I0ep-FgM-G8EyMy-ug"
 
-AToken = "sl.Av__rRKxl7D6OB63anvJVW6W-WP5NSQpiSUneoAmA3VoCBPjIDUAfTgMs-ap7wTU2iu0qKuhRo56N2Ca3SMQzlbu3OCQU7PQPBVSGUlZigLppSoouhGu7I0ep-FgM-G8EyMy-ug"
+    cloudStoring = TransferData(access_token)
 
-cloudStoring = TransferData(access_token)
+    while(os.path.isfile(file_from) == False):
+        fileFrom = input("Enter the file path to transfer:- ")
 
-while(os.path.isfile(file_from) == False):
-    fileFrom = input("Enter the file path to transfer:- ")
+    cloudStoring.upload_file(fileFrom)
 
-cloudStoring.upload_file(fileFrom)
+    print("File had been moved!)")
 
-print("File had been moved!)")
+if __name__ == '__main__':
+    main() 
