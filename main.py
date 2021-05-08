207abf88f7718715d52552ec75f8ed782fe1187e
Sanjay Srinivas <72558978+Sanjay567-coder@users.noreply.github.com>
2021-05-08 18:19:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,34 @@
-import dropbox
 import os
-
+import dropbox
 
 class TransferData:
     def __init__(self, access_token):
-        self.aT = access_token
+        self.atoken = access_token
 
     def upload_file(self, file_from, file_to):
-        dbx = dropbox.Dropbox(self.access_token)
-        fileName = os.path.split(file_from)[1]
+        dbx = dropbox.Dropbox(self.atoken)
 
-        dropbox_file = '/CloudStorage/'+fileName
+        for root,dirs,files in os.walk(file_from):
+            for fileName in files:
+                localpath = os.path.join(root,fileName)
+                relativepath = os.path.relpath(localpath,file_from)
+                dropboxpath = os.path.join(file_to,relativepath)
 
-        with open(file_from, "rb") as f:
-            dbx.files_upload(f.read(), dropbox_file,
-                            mode=dropbox.files.WriteMode.overwrite)
+                with open(localpath, "rb") as f:
+                    dbx.files_upload(f.read(), dropboxpath,
+                            mode=WriteMode('overwrite'))
 
 def main():
     
-    AToken = "sl.Av__rRKxl7D6OB63anvJVW6W-WP5NSQpiSUneoAmA3VoCBPjIDUAfTgMs-ap7wTU2iu0qKuhRo56N2Ca3SMQzlbu3OCQU7PQPBVSGUlZigLppSoouhGu7I0ep-FgM-G8EyMy-ug"
+    AToken = "sl.AwIeDKcJc1TG4rF8djcnjoif99W8NfnMHAPlPmRizJg0iFV8W3I06MPbzdyvElxBpE77CSf56f7fuKG1R3UJAlqqampYw-q68AqKA64CUCoa0MVrDBhnXrYRX7_QgGI0PsgQ-mQw"
 
-    cloudStoring = TransferData(access_token)
+    cloudStoring = TransferData(AToken)
 
-    while(os.path.isfile(file_from) == False):
-        fileFrom = input("Enter the file path to transfer:- ")
+    fileFrom = input("Enter the file path to transfer:- ")
 
-    cloudStoring.upload_file(fileFrom)
+    fileTo = input("Enter the file path to upload to dropbox:- ")
+
+    cloudStoring.upload_file(fileFrom, fileTo)
 
     print("File had been moved!)")
 
